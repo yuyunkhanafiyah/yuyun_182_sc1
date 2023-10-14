74fdbee7795f393dd54817b719950e135c994250
yuyunkhanafiyah <yuyunkh03@gmail.com>
2023-10-14 11:21:59 +0700

    practice4

---
+++ practice4/js/script1.js
@@ -0,0 +1,34 @@
+type="text/javascript"
+    window.alert('selamat datang di halaman embedded script');
+
+    function hitung_luas_segitiga(){
+        var a = document.getElementById("alas").value;
+        var t = document.getElementById("tinggi").value;
+        var hasil = 0.5 * a * t;
+        document.getElementById('hasil').innerHTML = hasil;
+    }
+    function konversi(){
+        var nilai_angka = document.getElementsByClassName("nilai_angka");
+        if(nilai_angka[0].value >= 90) grade = "A"
+        else if(nilai_angka[0].value >= 80) grade = "B+"
+        else if(nilai_angka[0].value >= 70) grade = "B"
+        else if(nilai_angka[0].value >= 60) grade = "C+"
+        else if(nilai_angka[0].value >= 50) grade = "C"
+        else if(nilai_angka[0].value >= 40) grade = "D"
+        else if(nilai_angka[0].value >= 30) grade = "E"                
+        else grade = "F"
+        document.getElementById('hasil2').innerHTML = grade;
+    }
+    function hitung_total(){
+        var jumlah = document.getElementById("jumlah").value;
+        var harga = document.getElementById("harga").value;
+        if(jumlah=="" || harga== ""){
+             document.getElementById('total').value = "0";
+        }
+        else{
+            document.getElementById('total').value = jumlah * harga;
+        }
+    }
+    function cetak(){
+        window.print();
+    }
